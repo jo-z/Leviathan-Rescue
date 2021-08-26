925f3442f8ea384ed558bfbf58a1d26cb0769e0f
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-08-26 16:44:26 -0400

    initial commit

---
+++ build/js/resources.js
@@ -0,0 +1,32 @@
+game.resources = [
+    {
+        "name": "area1",
+        "type": "tmx",
+        "src": "data/map/area1.tmx"
+    },
+    {
+        "name": "testMap",
+        "type": "tmx",
+        "src": "data/map/testMap.tmx"
+    },
+    {
+        "name": "forest tileset",
+        "type": "tsx",
+        "src": "data/map/forest tileset.tsx"
+    },
+    {
+        "name": "forest_background_1",
+        "type": "tsx",
+        "src": "data/map/forest_background_1.tsx"
+    },
+    {
+        "name": "forest_background_2",
+        "type": "tsx",
+        "src": "data/map/forest_background_2.tsx"
+    },
+    {
+        "name": "testSet",
+        "type": "tsx",
+        "src": "data/map/testSet.tsx"
+    }
+];
---
+++ data/map/forest tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="forest tileset" tilewidth="16" tileheight="16" tilecount="198" columns="18">
+ <image source="D:/itch/Tools/starstring-fields/tileset.png" width="288" height="176"/>
+</tileset>
